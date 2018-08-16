becd62383a257729fa2e28b5790acde35270b3d4
Mike Hole <mike@mikehole.com>
2018-08-16 08:44:48 +0100

    Add project files.

---
+++ EventFlowInLimuxContainer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Diagnostics.EventFlow;
+
+namespace EventFlowInLimuxContainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pipeline = DiagnosticPipelineFactory.CreatePipeline("eventFlowConfig.json");
+        }
+    }
+}
